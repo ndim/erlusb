3d3ff6bab986f25b75408f79a2b8386bca3d72da
Hans Ulrich Niedermann <hun@n-dimensional.de>
2009-01-25 23:18:46 +0100

    gps test: no libusb-1.0 context necessary

--- src/gps-comm-test.c
+++ src/gps-comm-test.c
@@ -5,21 +5,23 @@
 
 int main()
 {
-  libusb_context *ctx;
-  libusb_init(&ctx);
-  libusb_set_debug(ctx, 3);
-  libusb_device_handle *dev_handle =
-    libusb_open_device_with_vid_pid(NULL,
-				    0x091e /* Garmin */,
-				    0003 /* misc devices */);
+  libusb_init(NULL);
+  libusb_set_debug(NULL, 3);
+
+  libusb_device_handle *dev_handle = /* Garmin GPSmap (various models) */
+    libusb_open_device_with_vid_pid(NULL, 0x091e, 0003);
   assert(dev_handle != NULL);
+
   int claim = libusb_claim_interface(dev_handle, 0);
   assert(claim >= 0);
-  printf("ctx=%p dev_hdl=%p claim=%d\n",
-	 (void *)ctx, (void *)dev_handle, claim);
+
+  printf("dev_hdl=%p claim=%d\n", (void *)dev_handle, claim);
+
   int release = libusb_release_interface(dev_handle, 0);
   assert(release >= 0);
+
   printf("release=%d\n", release);
+
   libusb_close(dev_handle);
-  libusb_exit(ctx);
+  libusb_exit(NULL);
 }
